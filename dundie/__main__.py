3dfdd8b7f3273999151f12b3e3ddac21524ab894
alberto-soares <albertosoaress@gmail.com>
2025-02-20 10:41:05 -0300

    turned to installable binary #9

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,1 +1,6 @@
-print("Executing entry point for dundie... nova informacao")
+# print("Executing entry point for dundie... nova informacao")
+
+def main():
+    print("Executing dundie from entry point.")
+
+    
--- setup.py
+++ setup.py
@@ -11,7 +11,31 @@ setup(
     author="Alberto Luiz",
     #packages=["dundie"],
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+            #   |        | > nome do modulo callable apontado no programa
+            #   | > nome do binario     
+        ]
+    }
 )
+
+# *****************************************************
+# * Resultado apos executar python -m dundie e dundie * 
+# *****************************************************
+# 
+# (.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % 
+# python -m dundie
+# hello initializing dundie...
+# Executing entry point for dundie... nova informacao
+#
+#(.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % dundie
+# hello initializing dundie...
+# Executing dundie from entry point.
+#   
+
+
 # pyproject
 
 # external build tools (poetry, flit)
+ 
