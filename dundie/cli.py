4cf35f269e4c6f25ea4f288933d6446736d2c85e
alberto-soares <104383192+alberto-soares@users.noreply.github.com>
2025-12-10 13:39:59 -0300

    Alteracoes para a issue-10 relacionadas a testes¨ (#17)
    closes #10-completa

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,3 +1,4 @@
+"""Module"""
 # ***********
 # * Etapa 1 *
 # ***********
@@ -5,13 +6,15 @@
 import argparse
 # print("Executing entry point for dundie... nova informacao")
 
-#import sys
-#sys.argv # Recebe os parametros enviados do terminal
+# import sys
+# sys.argv # Recebe os parametros enviados do terminal
 
-from dundie.core import load # import absoluto
-#from .core import load # import relativo
+from dundie.core import load  # import absoluto
+# from .core import load  # import relativo
+
 
 def main():
+    """Metodo"""
     parser = argparse.ArgumentParser(
         description="Dunder Mifflin Rewards CLI",
         epilog="Enjoy and use with cautions.",
@@ -30,37 +33,35 @@ def main():
         default=None
     )
     args = parser.parse_args()
-    
-    #try:
-    #globals()[args.subcommand](args.filepath)
-    print(*globals()[args.subcommand](args.filepath), end="")
-    #|--> alteracao para corrigir o return no core.py
 
-    #except KeyError:
+    # try:
+    # globals()[args.subcommand](args.filepath)
+    print(*globals()[args.subcommand](args.filepath), end="")
+    # |--> alteracao para corrigir o return no core.py
+
+    # except KeyError:
     #    print("Subcommand is invalid.")
 
-    #print(args)
+    # print(args)
 #
 #
 # ********************************************************
 # * Execucao alteracao para corrigir o return no core.py *
 # ********************************************************
-#(.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % 
-# dundie load assets/people.csv 
-#Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com
+# (.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss %
+# dundie load assets/people.csv
+# Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com
 # Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com
-# Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com 
+# Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com
 #
-#(.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % 
+# (.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss %
 #
 # ********************
 # * Execucao Etapa 1 *
 # ********************
-# 
-#(.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % 
+#
+# (.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss %
 # dundie load people.csv
 # hello initializing dundie...
 # Namespace(subcommand='load', filepath='people.csv')
-
-
     # print("Executing dundie from entry point.")
