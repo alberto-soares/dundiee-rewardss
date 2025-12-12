17097afedb2a45139a56c8b2d5e5c1466f4b538c
alberto-soares <albertosoaress@gmail.com>
2025-12-12 14:47:55 -0300

    Adding finalyziers, setup and teardown to tests

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,3 +1,4 @@
+"""Module"""
 import pytest
 from subprocess import check_output
 
@@ -10,7 +11,7 @@ def test_load():
     out = check_output(
         ["dundie", "load", "tests/assets/people.csv"]
     ).decode("utf-8").split("\n")
-    #breakpoint()
+    # breakpoint()
     assert len(out) - 1 == 3
 #
 
@@ -26,11 +27,14 @@ def test_load():
 #            ).decode("utf-8").split("\n")
 #        #).decode("utf-8").split("\n")
 #        #breakpoint()
-#>       assert len(out) == 3
-#E       AssertionError: assert 1 == 3
-#E        +  where 1 = len(['Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com'])
+# >       assert len(out) == 3
+# E       AssertionError: assert 1 == 3
+# E        +  where 1 = len(
+# ['Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com
+#  Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com
+#  Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com'])
 #
-#integration/test_load.py:14: AssertionError
+# integration/test_load.py:14: AssertionError
 #
 #
 #
@@ -40,77 +44,100 @@ def test_load():
 #
 #    def test_load():
 #        """Test command load"""
-#    
+#
 #        out = check_output(
 #            ["dundie", "load", "tests/assets/people.csv"]
 #            ).decode("utf-8").split("\n")
 #        #).decode("utf-8").split("\n")
-#        #breakpoint()
-#>       assert len(out) == 3
-#E       AssertionError: assert 4 == 3
-#E        +  where 4 = len(['Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com', ' Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com', ' Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com ', ''])
+#        breakpoint()
+# >       assert len(out) == 3
+# E       AssertionError: assert 4 == 3
+# E        +  where 4 = len(
+# ['Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com',
+#  ' Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com',
+#  ' Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com ', ''])
+# integration/test_load.py:14: AssertionError
+# (.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss %
+# ipython
+# Python 3.11.5 (main, Sep 11 2023, 08:31:25) [Clang 14.0.6 ]
+# Type 'copyright', 'credits' or 'license' for more information
+# IPython 8.32.0 -- An enhanced Interactive Python. Type '?' for help.
 
-#integration/test_load.py:14: AssertionError
+# In [1]: from subprocess import check_output
 
+# In [3]: out = check_output(["dundie", "load", "tests/assets/people.csv"])
 
-
-#(.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % ipython
-#Python 3.11.5 (main, Sep 11 2023, 08:31:25) [Clang 14.0.6 ]
-#Type 'copyright', 'credits' or 'license' for more information
-#IPython 8.32.0 -- An enhanced Interactive Python. Type '?' for help.
-
-#In [1]: from subprocess import check_output
-
-#In [3]: out = check_output(["dundie", "load", "tests/assets/people.csv"])
-
-#In [5]: print(out)
-#b'Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com\n'
+# In [5]: print(out)
+# b'Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com
+#  Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com
+#  Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com\n'
 #
-#In [6]: out = check_output(["dundie", "load", "tests/assets/people.csv"]).decode("utf-8").split("\n")#
+# In [6]: out = check_output(["dundie", "load", "tests/assets/people.csv"]).
+# decode("utf-8").split("\n")#
 #
-#In [7]: print(out)
-#['Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com', '']
+# In [7]: print(out)
+# ['Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com
+#  Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com
+#  Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com', '']
 #
 # In [8]: check_output(["dundie", "load", "tests/assets/people.csv"]).decode()
-#Out[8]: 'Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com\n'
+# Out[8]: 'Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com
+#  Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com
+#  Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com\n'
 #
-#In [9]: check_output(["dundie", "load", "tests/assets/people.csv"]).decode("utf-8")
-#Out[9]: 'Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com\n'
+# In [9]: check_output(["dundie", "load", "tests/assets/people.csv"]).
+# decode("utf-8")
+# Out[9]: 'Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com
+#  Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com
+#  Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com\n'
 #
-#In [10]: check_output(["dundie", "load", "tests/assets/people.csv"]).decode("utf-8").split("\n")
-#Out[10]: 
-#['Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com',
+# In [10]: check_output(["dundie", "load", "tests/assets/people.csv"]).
+# decode("utf-8").split("\n")
+# Out[10]: 
+# ['Bruna Polliana, Presi, Presidente, bruna@dundlermifflin.com
+#  Arthur Santos, Presi, Vice-Presidente, arthur@dundlermifflin.com
+#  Victor Santos, Presi, Vice-Presidente, victor@dundlermifflin.com',
 # '']
-# 
-#(.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % ipython --profile=d5p02 --logfile=/tmp/5p
-#2.py
-#Activating auto-logging. Current session state plus future input saved.
-#Filename       : /tmp/5p2.py
-#Mode           : backup
-#Output logging : False
-#Raw input log  : False
-#Timestamping   : False
-#State          : active
-#Python 3.11.5 (main, Sep 11 2023, 08:31:25) [Clang 14.0.6 ]
-#Type 'copyright', 'credits' or 'license' for more information
-#IPython 8.32.0 -- An enhanced Interactive Python. Type '?' for help.
 #
-#IPython profile: d5p02
+# (.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss %
+# ipython --profile=d5p02 --logfile=/tmp/5p
+# 2.py
+# Activating auto-logging. Current session state plus future input saved.
+# Filename       : /tmp/5p2.py
+# Mode           : backup
+# Output logging : False
+# Raw input log  : False
+# Timestamping   : False
+# State          : active
+# Python 3.11.5 (main, Sep 11 2023, 08:31:25) [Clang 14.0.6 ]
+# Type 'copyright', 'credits' or 'license' for more information
+# IPython 8.32.0 -- An enhanced Interactive Python. Type '?' for help.
 #
-#In [1]: from subprocess import check_output
+# IPython profile: d5p02
 #
-#In [3]: check_output(["ls"])
-#Out[3]: b'LICENSE\nMakefile\nREADME.md\n__pycache__\nassets\nbuild\ndocs\ndundie\ndundie.egg-info\ndundie.log\nintegration\nmodulo1.py\nmodulo2.py\nmodulo3.py\nrequirements.dev.txt\nrequirements.test.txt\nrequirements.txt\nsetup.py\ntests\n'
+# In [1]: from subprocess import check_output
 #
-#In [4]: str(check_output(["ls"]))
-#Out[4]: "b'LICENSE\\nMakefile\\nREADME.md\\n__pycache__\\nassets\\nbuild\\ndocs\\ndundie\\ndundie.egg-info\\ndundie.log\\nintegration\\nmodulo1.py\\nmodulo2.py\\nmodulo3.py\\nrequirements.dev.txt\\nrequirements.test.txt\\nrequirements.txt\\nsetup.py\\ntests\\n'"
+# In [3]: check_output(["ls"])
+# Out[3]: b'LICENSE\nMakefile\nREADME.md\n__pycache__\nassets\nbuild\ndocs
+# ndundie\ndundie.egg-info\ndundie.log\nintegration\nmodulo1.py\nmodulo2.py
+# \nmodulo3.py\nrequirements.dev.txt\nrequirements.test.txt\nrequirements.txt\
+# nsetup.py\ntests\n'
 #
-#In [5]: check_output(["ls"]).decode()
-#Out[5]: 'LICENSE\nMakefile\nREADME.md\n__pycache__\nassets\nbuild\ndocs\ndundie\ndundie.egg-info\ndundie.log\nintegration\nmodulo1.py\nmodulo2.py\nmodulo3.py\nrequirements.dev.txt\nrequirements.test.txt\nrequirements.txt\nsetup.py\ntests\n'
+# In [4]: str(check_output(["ls"]))
+# Out[4]: "b'LICENSE\\nMakefile\\nREADME.md\\n__pycache__\\nassets
+# \\nbuild\\ndocs\\ndundie\\ndundie.egg-info\\ndundie.log
+# \\nintegration\\nmodulo1.py\\nmodulo2.py\\nmodulo3.py
+# \\nrequirements.dev.txt\\nrequirements.test.txt\\nrequirements.txt
+# \\nsetup.py\\ntests\\n'"
 #
-#In [6]: check_output(["ls"]).decode("utf-8").split("\n")
-#Out[6]: 
-#['LICENSE',
+# In [5]: check_output(["ls"]).decode()
+# Out[5]: 'LICENSE\nMakefile\nREADME.md\n__pycache__\nassets\nbuild\ndocs\
+# ndundie\ndundie.egg-info\ndundie.log\nintegration\nmodulo1.py\nmodulo2.py\nmodulo3.py\
+# nrequirements.dev.txt\nrequirements.test.txt\nrequirements.txt\nsetup.py\ntests\n'
+#
+# In [6]: check_output(["ls"]).decode("utf-8").split("\n")
+# Out[6]:
+# ['LICENSE',
 # 'Makefile',
 # 'README.md',
 # '__pycache__',
@@ -130,5 +157,5 @@ def test_load():
 # 'setup.py',
 # 'tests',
 # '']
-# 
+#
 #  In [7]: exit
