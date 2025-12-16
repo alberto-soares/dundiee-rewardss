aea765c507c0056cc10dddfb6c4adc99fd5c2bba
alberto-soares <albertosoaress@gmail.com>
2025-12-16 10:32:19 -0300

    linters completa add isort, black e outros

--- modulo2.py
+++ modulo2.py
@@ -1,21 +1,25 @@
+"""Module"""
 # Parte 1
- 
-import modulo1 # Importa a funcao say_hi do modulo1 que esta na mesma pasta
 
-#modulo1.say_hi(modulo1.NOME)
+import modulo1  # Importa a funcao say_hi do modulo1 que esta na mesma pasta
+
+# modulo1.say_hi(modulo1.NOME)
 
 # *********************
 # * Resultado Parte 1 *
 # *********************
 
-#(.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % 
+# (.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss %
 # pytmodulo2.py
 # Alberto
 
 # Parte 2
 
+
 def say_hello(n):
+    """Module"""
     print(f"Hello {n}")
+
 
 modulo1.say_hi(modulo1.NOME)
 
@@ -25,7 +29,7 @@ say_hello(modulo1.NOME)
 # * Resultado Parte 2 *
 # *********************
 
-#(.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss % 
+# (.venv) (base) albertosoares@MacBook-Pro-de-Alberto dundiee-rewardss %
 # python modulo2.py
-#Hi Alberto * Esta vindo do modulo1
-#Hello Alberto * Esta vindo do modulo2
+# Hi Alberto * Esta vindo do modulo1
+# Hello Alberto * Esta vindo do modulo2
